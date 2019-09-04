5dda8b35bfc7c6b659a32ffdfe7d3116368fc6f3
comenice <571503431@qq.com>
2019-09-04 17:26:39 +0800

    项目开始搭建

--- springcloud/springcloud-config-server/src/main/java/com/_1813qt/learn/springcloudconfigserver/SpringcloudConfigServerApplication.java
+++ springcloud/springcloud-config-server/src/main/java/com/_1813qt/learn/springcloudconfigserver/SpringcloudConfigServerApplication.java
@@ -2,8 +2,10 @@ package com._1813qt.learn.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringcloudConfigServerApplication {
 
     public static void main(String[] args) {
